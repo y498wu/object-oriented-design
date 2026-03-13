ca46a43b96ef1ce34284f15ff1d0bfeabe19e0aa
Yeshu Wu <y498wu@uwaterloo.ca>
2026-03-12 21:29:53 -0700

    working version v2 for mutex-shared-counter

--- async-task-queue/mutex-shared-counter/src/Counter.java
+++ async-task-queue/mutex-shared-counter/src/Counter.java
@@ -1,16 +1,22 @@
 public class Counter {
     static int count = 0;
 
-    private static synchronized void addTenThousand(){
-        for (int i = 0; i < 100000; i++){
-            count++;
-        }
+    private static synchronized void inc(){
+        count++;
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(Counter::addTenThousand);
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++) {
+                Counter.inc();;
+            }
+        });
 
-        Thread t2 = new Thread(Counter::addTenThousand);
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100000; i++) {
+                Counter.inc();
+            }
+        });
 
         t1.start();
         t2.start();
